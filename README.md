7016aeefb4bbb6b56428ff8176387b9d3efd9e73
Joao Marcos Teixeira Ribeiro <jima.teixeira10@gmail.com>
2022-08-24 13:22:05 -0300

    finalizando projeto

---
+++ README.md
@@ -0,0 +1,33 @@
+# Frontend Mentor - QR code component solution
+
+This is a solution to the [QR code component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/qr-code-component-iux_sIO_H). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+- See hover states for interactive elements
+
+### Links
+
+- Solution URL: [QR code component solution]()
+- Live Site URL: [GitHub Pages]()
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS
+- JavaScript
---
+++ js/main.js
@@ -0,0 +1,47 @@
+const btn1 = document.querySelector('.colorful')
+const btn2 = document.querySelector('.bw')
+const body = document.querySelector('body')
+const tg1 = document.querySelector('.tg-colorful')
+const tg2 = document.querySelector('.tg-bw')
+
+btn1.addEventListener('click', () => {
+
+    if(body.classList.contains("theme-2")) {
+        body.classList.remove('theme-2')
+        btn2.children[0].classList.remove('arrow-rotate')
+        btn2.parentElement.classList.remove('theme-active')
+        btn2.classList.remove('btn-active')
+    }
+    btn1.children[0].classList.toggle('arrow-rotate')
+    btn1.parentElement.classList.toggle('theme-active')
+    btn1.classList.add('btn-active')
+    tg1.classList.toggle("tg1")
+    tg2.textContent = 'off'
+    if(tg1.textContent == 'on') {
+        tg1.textContent = 'off'
+    }else {
+        tg1.textContent = 'on'
+    }
+    body.classList.toggle('theme-1')
+})
+
+btn2.addEventListener('click', () => {
+    
+    if(body.classList.contains("theme-1")) {
+        body.classList.remove('theme-1')
+        btn1.children[0].classList.remove('arrow-rotate')
+        btn1.parentElement.classList.remove('theme-active')
+        btn1.classList.remove('btn-active')
+    }
+    btn2.children[0].classList.toggle('arrow-rotate')
+    btn2.parentElement.classList.toggle('theme-active')
+    btn2.classList.add('btn-active')
+    tg2.classList.toggle("tg2")
+    tg1.textContent = 'off'
+    if(tg2.textContent == 'on') {
+        tg2.textContent = 'off'
+    }else {
+        tg2.textContent = 'on'
+    }
+    body.classList.toggle('theme-2')
+})
